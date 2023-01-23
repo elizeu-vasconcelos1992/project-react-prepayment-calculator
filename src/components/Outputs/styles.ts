49145d0e3f7183e4ec19c2130b9af4ccd04b52cc
unknown <elizeucosta21@gmail.com>
2023-01-22 23:46:37 -0300

    feat: added put-page components

---
+++ src/components/Outputs/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledOutPut = styled.div`
+  width: 80%;
+  height: 75%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`;
