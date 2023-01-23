46d1e8c02c030fb1c0a5de81c858557d82a84686
unknown <elizeucosta21@gmail.com>
2023-01-22 23:47:23 -0300

    feat: added output page

---
+++ src/components/OutputsPages/index.tsx
@@ -0,0 +1,10 @@
+import OutPuts from "../Outputs";
+import { StyledOutPutPage } from "./styles";
+
+export default function OutPutPage() {
+  return (
+    <StyledOutPutPage>
+      <OutPuts />
+    </StyledOutPutPage>
+  );
+}
